682e9cf1293bbf7f20e27c11c4e21a7170b72a08
y616388 <56483314+y616388@users.noreply.github.com>
2019-11-05 21:19:46 +0100

    Add files via upload

--- public/dania.js
+++ public/dania.js
@@ -1,14 +1,62 @@
+var pizza_img = "http://tattoopizza.pl/wp-content/uploads/2016/10/wm1p-300x300.png";
 var pizze = [
-	"Margherita",
-	"Gyros",
-	"Tommo",
-	"Capriciosa",
-	"Hawajska",
-	"Barbado",
-	"Kebab",
-	"Carbonara",
-	"Mafioso",
-	"Santiago",
-	"Ognista ",
-	"Góralska"
+	['Margherita', pizza_img, 20, 'sos pomidorowy, ser, oregano'],
+	['Gyros', pizza_img, 22, 'sos gyros, ser, kurczak kebab, cebula'],
+	['Capriciosa', pizza_img, 24, 'sos, ser, szynka, pieczarki'],
+	['Tommo', pizza_img, 24, 'sos, ser, kurczak, papryka'],
+	['Hawajska', pizza_img, 24, 'sos, ser, szynka, ananas'],
+	['Barbado', pizza_img, 28, 'sos, ser, kurczak kebab, ananas'],
+	['Kebab', pizza_img, 28, 'sos gyros, ser, kurczak kebab, ogórek konserwowy'],
+	['Carbonara', pizza_img, 28, 'sos śmietanowy, ser, bekon, pieczarki, cebula'],
+	['Mafioso', pizza_img, 28, 'sos, ser, salami, oliwki zielone, piri piri'],
+	['Santiago', pizza_img, 30, 'sos, ser, szynka, ananas, kukurydza'],
+	['Ognista ', pizza_img, 30, 'sos ostry, ser, salami, piri piri, oliwki zielone, tabasco'],
+	['Góralska', pizza_img, 30, 'sos bbq, ser, bekon, cebula, ser wędzony, żurawina']
 ];
+
+var miesne = [
+	['Filet z kurczaka', 'img/.png', 25, ''],
+	['Kotlet drobiowy', 'img/.png', 25, 'w złocistej panierce'],
+	['Zestaw Fitness', 'img/.png', 25, 'filet z grilla na warzywach z ryżem'],
+	['Fileciki drobiowe duszone', 'img/.png', 25, 'w sosie śmietanowo-kurkowym'],
+	['Filet Hawajski', 'img/.png', 25, 'z grillowanym ananasem'],
+	['Risotto z kurczakiem', 'img/.png', 25, 'w delikatnym sosie śmietanowo-serowym']
+];
+
+var makarony = [
+	['Pasta z kurczakiem', 'img/.png', 20, ''],
+	['Pasta Bolognese', 'img/.png', 20, ''],
+	['Pasta serowa', 'img/.png', 20, ''],
+	['Pasta carbonara', 'img/.png', 20, ''],
+	['Ravioli z serem', 'img/.png', 20, 'podawane z kiełbasą chorizo i cukinią'],
+	['Ravili z serem ricotta', 'img/.png', 20, 'w sosie szczypiorkowo-cytrynowym']
+];
+
+var dodatki = [
+	['Sos pomidorowy', 'img/.png', 2, ''],
+	['Sos czosnkowy', 'img/.png', 2, ''],
+	['Sos pieprzowy-ostry', 'img/.png', 2, ''],
+	['Sos BBQ', 'img/.png', 2, ''],
+	['Sos słodko-kwaśny', 'img/.png', 2, ''],
+	['Frytki', 'img/.png', 5, '']
+];
+
+var alkohole = [
+	['Dzik .4l', 'img/.png', 6, ''],
+	['Dzik .5l', 'img/.png', 8, ''],
+	['Dzik 0% .4l', 'img/.png', 0, ''],
+	['Esperados .4l', 'img/.png', 8, ''],
+	['Mech .4l', 'img/.png', 6, ''],
+	['Mech .5l', 'img/.png', 8, '']
+];
+
+var napoje = [
+	['Cepsi .5l', 'img/.png', 6, ''],
+	['Cepsi 1l', 'img/.png', 8, ''],
+	['Pola .5l', 'img/.png', 6, ''],
+	['Pola 1l', 'img/.png', 8, ''],
+	['Woda .5l', 'img/.png', 5, ''],
+	['Irinda 1l', 'img/.png', 8, '']
+];
+
+var dania = [pizze, miesne, makarony, dodatki, alkohole, napoje];
--- public/strona.js
+++ public/strona.js
@@ -1,37 +1,118 @@
 start();
 function start() {
-	document.getElementById('kontener').innerHTML+="<div id='dania'></div>";
-	document.getElementById('kontener').innerHTML+="<div id='baner'>baner</div>";
-	document.getElementById('kontener').innerHTML+="<div id='baner2'></div>";
-	document.getElementById('kontener').innerHTML+="<div id='baner3'></div>";
-	document.getElementById('kontener').innerHTML+="<div id='menu'><div class='przycisk' id='o1' onclick='opcja(1)'>Pizza</div><div class='przycisk' id='o2' onclick='opcja(2)'>Dania Mięsne</div><div class='przycisk' id='o3' onclick='opcja(3)'>Makarony</div><div class='przycisk' id='o4' onclick='opcja(4)'>Dodatki</div><div class='przycisk' id='o5' onclick='opcja(5)'>Alkohole</div><div class='przycisk' id='o6' onclick='opcja(6)'>Napoje</div></div>";
-	document.getElementById('kontener').innerHTML+="<div id='main'></div>";
-	document.getElementById('kontener').innerHTML+="<div id='koszyk_open'>Przejdź do koszyka</div>";
-	document.getElementById('kontener').innerHTML+="<div id='promocje'></div>";
-	
+	document.getElementById('kontener').innerHTML += "<div id='dania'></div>";
+	document.getElementById('kontener').innerHTML += "<div id='baner'>baner</div>";
+	document.getElementById('kontener').innerHTML += "<div id='baner2'></div>";
+	document.getElementById('kontener').innerHTML += "<div id='baner3'></div>";
+	document.getElementById('kontener').innerHTML += "<div id='menu'><div class='przycisk' id='o0' onclick='opcja(0)'>Pizza</div><div class='przycisk' id='o1' onclick='opcja(1)'>Dania Mięsne</div><div class='przycisk' id='o2' onclick='opcja(2)'>Makarony</div><div class='przycisk' id='o3' onclick='opcja(3)'>Dodatki</div><div class='przycisk' id='o4' onclick='opcja(4)'>Alkohole</div><div class='przycisk' id='o5' onclick='opcja(5)'>Napoje</div></div>";
+	document.getElementById('kontener').innerHTML += "<div id='main'></div>";
+	document.getElementById('kontener').innerHTML += "<div id='koszyk_open'>Przejdź do koszyka➜</div>";
+	document.getElementById('kontener').innerHTML += "<div id='promocje'></div>";
+  document.getElementById('kontener').innerHTML += "<div id='dodanie_dania_tlo' onclick='zamkniecie_dodania()'></div>"
+  document.getElementById('kontener').innerHTML += "<div id='dodanie_dania'></div>"
 }
 
-var id = 1;
+var id = 0;
+var otwarte = -1;
 
 function opcja(id) {
-	for(var i = 1; i<=6; i++) {
-		document.getElementById('o'+i).style.background = "#C68A66";
+	for(var i = 0; i <= 5; i++) {
+		document.getElementById('o' + i).style.background = "#C68A66";
 	}
-	document.getElementById('o'+id).style.background = "#a3633d";
-	
-	if(id == 1) {
-		//console.log("a");
-		for(var i = 1; i<=12; i++) {
-			document.getElementById('dania').innerHTML+="<div class='danie'>"+
-			"<div class='danie_nazwa_tlo'></div><div class='danie_nazwa'>"+pizze[i-1]+"</div>"+
-			"<img src='http://tattoopizza.pl/wp-content/uploads/2016/10/wm1p-300x300.png'></div>";
+	document.getElementById('o' + id).style.background = "#a3633d";
+
+  document.getElementById('dania').innerHTML = "";
+
+  otwarte = -1;
+
+	if(id == 0) {
+		for(var i = 0; i <= 11; i++) {
+			document.getElementById('dania').innerHTML += "<div class='danie' onclick = 'otworz("+i+")'>" +
+			"<div class='danie_nazwa_tlo' id='danie_nazwa_tlo" + i + "'></div>" +
+      "<div class='danie_nazwa' id='danie_nazwa" + i + "'>" + dania[id][i][0] + "</div>" +
+      "<div class='danie_opis' id='danie_opis" + i + "'>" + dania[id][i][3] + "</div>" +
+      "<div class='danie_dodaj' id='danie_dodaj" + i + "' onclick='dodaj("+id+","+i+")'>Dodaj do koszyka</div>" +
+			"<img src='" + dania[id][i][1] + "'></div>";
 		}
-	}
+	}else {
+    for(var i = 0; i <= 5; i++) {
+			document.getElementById('dania').innerHTML += "<div class='danie' onclick = 'otworz(" + i + ")'>" +
+			"<div class='danie_nazwa_tlo' id='danie_nazwa_tlo" + i + "'></div>" +
+      "<div class='danie_nazwa' id='danie_nazwa" + i + "'>" + dania[id][i][0] + "</div>" +
+      "<div class='danie_opis' id='danie_opis" + i + "'>" + dania[id][i][3] + "</div>" +
+      "<div class='danie_dodaj' id='danie_dodaj" + i + "' onclick='dodaj("+id+","+i+")'>Dodaj do koszyka</div>" +
+			"<img src='" + dania[id][i][1] + "'></div>";
+		}
+  }
 }
 
-opcja(1);
+opcja(0);
 
 //<div id='koszyk_niego'>Do zapłaty: 12,50zł</div>
 //<div id='koszyk_go'>Zapłać</div>
 
+function otworz(id) {
+  if(otwarte != -1) {
+    document.getElementById("danie_nazwa" + otwarte).style.animation = "nazwa_dol 0.1s linear forwards";
+    document.getElementById("danie_nazwa_tlo" + otwarte).style.animation = "nazwa_tlo_dol 0.1s linear forwards";
 
+    document.getElementById("danie_opis" + otwarte).style.display = "none";
+    document.getElementById("danie_dodaj" + otwarte).style.display = "none";
+    document.getElementById("danie_opis" + otwarte).style.animation = "znikanie 0.1s linear forwards";
+    document.getElementById("danie_dodaj" + otwarte).style.animation = "znikanie 0.1s linear forwards";
+  }
+  if(id != otwarte) {
+    document.getElementById("danie_nazwa" + id).style.animation = "nazwa_gora 0.1s linear forwards";
+    document.getElementById("danie_nazwa_tlo" + id).style.animation = "nazwa_tlo_gora 0.1s linear forwards";
+
+    document.getElementById("danie_opis" + id).style.display = "block";
+    document.getElementById("danie_dodaj" + id).style.display = "block";
+    document.getElementById("danie_opis" + id).style.animation = "odznikanie 0.1s linear forwards";
+    document.getElementById("danie_dodaj" + id).style.animation = "odznikanie 0.1s linear forwards";
+    otwarte = id;
+  }else {
+    otwarte = -1;
+  }
+}
+
+function dodaj(typ, id) {
+  document.getElementById("dodanie_dania").style.display = "block";
+  document.getElementById("dodanie_dania_tlo").style.display = "block";
+  document.getElementById("dodanie_dania").style.animation = "odznikanie 0.1s linear forwards";
+  document.getElementById("dodanie_dania_tlo").style.animation = "odznikanie2 0.1s linear forwards";
+
+  document.getElementById("dodanie_dania").innerHTML = "";
+
+  if(typ == 0) {
+    //document.getElementById("dodanie_dania").innerHTML += dania[typ][id][0] + "<br>" + dania[typ][id][2] + "zł";
+    document.getElementById("dodanie_dania").innerHTML += "<div id='dodanie_nazwa'>"+dania[typ][id][0]+"</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>30cm</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>"+dania[typ][id][2]+"zł</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>+</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_puste'>0</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>-</div>";
+
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>40cm</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>"+(dania[typ][id][2]) * 1.5+"zł</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>+</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_puste'>0</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>-</div>";
+
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>50cm</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kolo'>"+(dania[typ][id][2]) * 2+"zł</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>+</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_puste'>0</div>";
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_kwadrat'>-</div>";
+
+    document.getElementById("dodanie_dania").innerHTML += "<div class='dodanie_przycisk'>Dodaj do koszyka</div>";
+  }
+}
+
+function zamkniecie_dodania() {
+  document.getElementById("dodanie_dania").style.animation = "znikanie 0.1s linear forwards";
+  document.getElementById("dodanie_dania_tlo").style.animation = "znikanie2 0.1s linear forwards";
+  setTimeout(function() {
+    document.getElementById("dodanie_dania").style.display = "none";
+    document.getElementById("dodanie_dania_tlo").style.display = "none";
+  }, 100);
+}
