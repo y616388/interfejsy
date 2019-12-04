d2fa46f8f06f2aefafa0f0520884ca45af719202
y616388 <56483314+y616388@users.noreply.github.com>
2019-12-04 13:56:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 var express = require('express');
-//var socket = require('socket.io');
 
-//setup
 var app = express();
 var port = process.env.PORT || 5000
 var server = app.listen(port, function() {
 	console.log('start ' + port);
 });
 
-
-//pliki
 app.use(express.static('public'));
-/*
-var io = socket(server);
-
-//polaczenie
-io.on('connection', function(socket) {
-	console.log('made socket connection', socket.id);
-
-	//zapytanie
-	socket.on('msg', function(data) {
-		io.sockets.emit('msg', data);
-	});
-});
-*/
